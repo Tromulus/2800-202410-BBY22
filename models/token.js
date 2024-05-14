f42697242735da55714d1f687be75e678526588f
Carol Li <lizhi.2496@gmail.com>
2024-05-14 02:05:13 -0700

    Change the schema in token.js to userId.

--- models/token.js
+++ models/token.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const tokenSchema = new Schema({
-  email: {
-    type: String,
+  userId: {
+    type: Schema.Types.ObjectId,
     required: true,
   },
   token: {
