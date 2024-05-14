7796c09581f0ae77fd82c4240264a9c523b9df4e
Thomas Smith <thomassmith@live.ca>
2024-05-14 15:02:13 -0700

    Fixed session not being store in the database by updating databaseConnection.js

--- public/js/databaseConnection.js
+++ public/js/databaseConnection.js
@@ -2,13 +2,15 @@ require('dotenv').config();
 const session = require('express-session');
 const mongoose = require('mongoose');
 const MongoDBSession = require('connect-mongodb-session')(session);
+const mongodb_database = process.env.MONGODB_DATABASE;
 const mongodb_host = process.env.MONGODB_HOST;
 const mongodb_user = process.env.MONGODB_USER;
 const mongodb_password = process.env.MONGODB_PASSWORD;
-const atlasURI = `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/?retryWrites=true`;
+const atlasURI = `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/${mongodb_database}?retryWrites=true`;
 var database = mongoose.connect(atlasURI).then(console.log("MongoDB connected"));
 const store = new MongoDBSession({
     uri: atlasURI,
     collection: 'sessions'
 });
-module.exports = {database};
+module.exports.database = database;
+module.exports.store = store;
