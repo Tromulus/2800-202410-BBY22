b74f799bcbea90d898182cd8e2ca43f96ede62d1
Thomas Smith <thomassmith@live.ca>
2024-05-22 20:29:27 -0700

    1) Updated image for 'Aegis VII' 2) Included price in robot schema and regenerated robots in database to include this field 3) Added schema for user cart 4) Added cart functionality using session to keep cart active until logout 5) Changed robot details from linking to another page, to opening a modal 6) Added template for checkout page

---
+++ models/cart.js
@@ -0,0 +1,24 @@
+module.exports = function Cart(oldCart) {
+    this.models = oldCart.models || {};
+    this.totalQty = oldCart.totalQty || 0;
+    this.totalPrice = oldCart.totalPrice || 0;
+
+    this.add = function(item) {
+        var storedModel = this.models[item.model];
+        if (!storedModel) {
+            storedModel = this.models[item.model] = {model: item.model, qty: 0, price: item.price};
+        }
+        storedModel.qty++;
+        storedModel.price = storedModel.qty * item.price;
+        this.totalQty++;
+        this.totalPrice += item.price;
+    }
+
+    this.generateArray = function() {
+        var arr = [];
+        for (var id in this.items) {
+            arr.push(this.items[id]);
+        }
+        return arr;
+    }
+};
