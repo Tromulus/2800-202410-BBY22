49001a0fe6f34f3f04f0d2d6ed76b9fbc573f87e
Nathan Mak <nmak18@my.bcit.ca>
2024-05-29 00:00:52 -0700

    Updated the province input field in both profile and signup pages to use a dropdown menu and added additional validation for profile saved inputs.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,48 +1,44 @@
-
 // importing objects from authServices
-const { signup, login } = require('../services/authService');
+const { signup, login } = require("../services/authService");
 
 const signUpController = async (req, res, next) => {
-    try {
-        await signup(req.body);
-        req.session.authenticated = true;
-        req.session.username = req.body.username;
-        req.session.cookie.maxAge = 1000 * 60 * 60 * 24;
-        return res.redirect('/robots');
-        
-    } catch (error) {
-        next(error);
-    }
+  try {
+    await signup(req.body);
+    req.session.authenticated = true;
+    req.session.username = req.body.username;
+    req.session.cookie.maxAge = 1000 * 60 * 60 * 24;
+    return res.redirect("/robots");
+  } catch (error) {
+    next(error);
+  }
 };
 
 const loginController = async (req, res, next) => {
-    try {
-        await login(req.body);
-        req.session.authenticated = true;
-        req.session.username = req.body.username;
-        req.session.cookie.maxAge = 1000 * 60 * 60 * 24;
-        return res.redirect('/robots');
-    } catch (error) {
-        console.error('Error: ', error.message);
-        res.render('loginError', {errorMessage: error.message});
-    }
+  try {
+    await login(req.body);
+    req.session.authenticated = true;
+    req.session.username = req.body.username;
+    req.session.cookie.maxAge = 1000 * 60 * 60 * 24;
+    return res.redirect("/robots");
+  } catch (error) {
+    console.error("Error: ", error.message);
+    res.render("loginError", { errorMessage: error.message });
+  }
 };
 
 const logoutController = (req, res) => {
-    console.log("Logging out.");
-    req.session.destroy();
-    res.redirect("/");
+  console.log("Logging out.");
+  req.session.destroy();
+  res.redirect("/");
 };
 
 module.exports = {
-    signUpController,
-    loginController,
-    logoutController
+  signUpController,
+  loginController,
+  logoutController,
 };
 
-
 // --------------------- < Copied from ResetPassword Website > ----------------------- //
-
 
 // const {
 //     signup,
--- index.js
+++ index.js
@@ -101,9 +101,9 @@ app.get("/surprise", (req, res) => {
 
 app.get('/profile', async (req, res) => {
     if (req.session.authenticated) {
-        console.log("in profile");
+        // console.log("in profile");
         const result = await User.findOne({ username: req.session.username });
-        console.log(result);
+        // console.log(result);
 
         if (!result) {
             res.send("User not found");
@@ -118,16 +118,46 @@ app.get('/profile', async (req, res) => {
 
 app.post('/update/:email', async (req, res) => {
     var email = req.params.email;
+    var address = req.body.address;
+    var city = req.body.city;
+    var province = req.body.province;
+    var postal = req.body.postal;
 
     const schema = Joi.object({
-        email: Joi.string().email(),
+        email: Joi.string()
+        .email({tlds: {allow: false}})
+        .required()
+        .messages({
+            'string.base': 'Email must be a string',
+            'string.empty': 'Email is required',
+            'string.email': 'Email must be a valid email',
+            'string.domain': 'Email must be a valid email'
+        }),
+        address: Joi.string().trim().required().messages({
+            'any.required': 'Address is required.',
+            'string.empty': 'Address cannot be empty.'
+        }),
+        city: Joi.string().trim().required().messages({
+            'any.required': 'City is required.',
+            'string.empty': 'City cannot be empty.'
+        }),
+        province: Joi.string().trim().required().messages({
+            'any.required': 'Province is required.',
+            'string.empty': 'Province cannot be empty.'
+        }),
+        postal: Joi.string().trim().regex(/^[A-Za-z]\d[A-Za-z][ -]?\d[A-Za-z]\d$/)
+            .required().messages({
+                'any.required': 'Postal code is required.',
+                'string.empty': 'Postal code cannot be empty.',
+                'string.pattern.base': 'Postal code must be a valid Canadian postal code.'
+            })
     });
 
-    const validationResult = schema.validate({ email });
+    const validationResult = schema.validate({ email, address, city, province, postal }, { abortEarly: false });
     if (validationResult.error != null) {
-        console.log(validationResult.error);
-        res.redirect('/login');
-        return;
+    let errorMessages = validationResult.error.details.map(detail => detail.message);
+    res.redirect('/profile?errors=' + encodeURIComponent(JSON.stringify(errorMessages)));
+    return;
     }
 
     await User.updateOne({ email: email }, 
